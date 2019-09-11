38c159d81c35c0818c53b83c19e0b508cb6d4f89
user <artbiba@ukr.net>
2019-09-11 18:14:35 +0300

    .

---
+++ README.md
@@ -0,0 +1,11 @@
+Hiptest/Selenium-samples
+========================
+[![Build Status](https://travis-ci.org/hiptest/selenium-sample.svg)](https://travis-ci.org/hiptest/selenium-sample)
+
+This is a simple example on how to automate tests written in [Hiptest](http://hiptest.net) with Java and Selenium.
+This project is the base for [this tutorial on Hiptest blog](http://blog.hiptest.net/2015/10/30/tutorial-a-simple-selenium-test-from-hiptest/)
+
+Acknowledgments
+---------------
+
+A lot of thanks to [Ardesco/Selenium-Maven-Template](https://github.com/Ardesco/Selenium-Maven-Template) who made it possible to easily get a Maven build working with Selenium without having to fight for days to get anything working.
---
+++ src/test/java/com/example/SeleniumTest.java
@@ -0,0 +1,44 @@
+package com.example;
+
+import org.testng.annotations.*;
+
+public class SeleniumTest {
+
+    public Actionwords actionwords;
+
+    @BeforeMethod
+    public void setUp() {
+        actionwords = new Actionwords();
+    }
+
+    @Test
+    public void findingASiteOnGoogleUid04058d8a519848d9a423df387c4bd368() {
+        // Given I open "http://google.com"
+        actionwords.iOpenHttpGoogleCom("http://google.com");
+        // When I search for "Hiptest"
+        actionwords.iSearchForHiptest("Hiptest");
+        // Then a link to "Hiptest: Agile test management tool & behavior driven development"
+        actionwords.aLinkToHiptestAgileTestManagementToolBehaviorDrivenDevelopment("hiptest");
+    }
+
+    @Test
+    public void loginToAccountWithValidValuesUid5585c7a8d44c48a289b88774b5f34269() {
+        // Given I open page
+        actionwords.iOpenPage("https://test.loadedreports.com/");
+        // And click login button
+        actionwords.clickLoginButton();
+        // When I input valid email
+        actionwords.iInputValidEmail("evgeniia.vecheria@redwerk.com");
+        // And input valid password
+        actionwords.inputValidPassword("ZK13+bbf70");
+        // And click on Login button
+        actionwords.clickOnLoginButton();
+        // Then the url contains
+        actionwords.theUrlContains("/dashboard");
+    }
+
+    @AfterMethod
+    public void stop(){
+        actionwords.getDriver().quit();
+    }
+}
