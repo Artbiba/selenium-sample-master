453f874b7c9b3165d99c62465e21aace11b713ad
user <artbiba@ukr.net>
2019-09-11 20:39:27 +0300

    asd

--- src/test/java/com/example/SeleniumTest.java
+++ src/test/java/com/example/SeleniumTest.java
@@ -12,17 +12,17 @@ public class SeleniumTest {
     }
 
     @Test
-    public void findingASiteOnGoogle() {
+    public void findingASiteOnGoogleUid04058d8a519848d9a423df387c4bd368() {
         // Given I open "http://google.com"
         actionwords.iOpenHttpGoogleCom("http://google.com");
         // When I search for "Hiptest"
         actionwords.iSearchForHiptest("Hiptest");
         // Then a link to "Hiptest: Agile test management tool & behavior driven development"
-        actionwords.aLinkToHiptestAgileTestManagementToolBehaviorDrivenDevelopment("hiptest");
+        actionwords.aLinkToHiptestAgileTestManagementToolBehaviorDrivenDevelopment("Hiptest: Agile test management tool & behavior driven development");
     }
 
     @Test
-    public void loginToAccountWithValidValues() {
+    public void loginToAccountWithValidValuesUid5585c7a8d44c48a289b88774b5f34269() {
         // Given I open page
         actionwords.iOpenPage("https://test.loadedreports.com/");
         // And click login button
@@ -38,7 +38,7 @@ public class SeleniumTest {
     }
 
     @Test
-    public void newScenario() {
+    public void newScenarioUide04443b80bf8487e93201538a1d3c199() {
         // When go to page
         actionwords.goToPage();
         // When go to England
@@ -48,7 +48,7 @@ public class SeleniumTest {
     }
 
     @Test
-    public void new2() {
+    public void new2Uid6009eb5bc9c14e6a8029ff79a0ff2532() {
         actionwords.goToPage2();
         actionwords.goToEngland2();
     }
